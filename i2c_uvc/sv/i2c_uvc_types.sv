3fd90734c9090d798f22f90ebd6dff127bdceddf
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-20 13:01:56 +0200

    chore: change variable names for i2c

--- i2c_uvc/sv/i2c_uvc_types.sv
+++ i2c_uvc/sv/i2c_uvc_types.sv
@@ -1,24 +1,20 @@
-`ifndef GPIO_UVC_TYPES_SV
-`define GPIO_UVC_TYPES_SV
-
-parameter int GPIO_UVC_MAX_WIDTH = 32;
-typedef logic [GPIO_UVC_MAX_WIDTH-1:0] gpio_uvc_data_t;
+`ifndef I2C_UVC_TYPES_SV
+`define I2C_UVC_TYPES_SV
 
 typedef enum {
-  GPIO_UVC_ITEM_FIRST, 
-  GPIO_UVC_ITEM_MIDDLE, 
-  GPIO_UVC_ITEM_LAST
-} gpio_uvc_item_stage_e;
+  I2C_UVC_ITEM_FIRST, 
+  I2C_UVC_ITEM_MIDDLE, 
+  I2C_UVC_ITEM_LAST
+} i2c_uvc_item_stage_e;
 
 typedef enum {
-  GPIO_UVC_ITEM_SYNC,
-  GPIO_UVC_ITEM_ASYNC
-} gpio_uvc_item_type_e;
+  I2C_UVC_ITEM_SYNC,
+  I2C_UVC_ITEM_ASYNC
+} i2c_uvc_item_type_e;
 
 typedef enum {
-  GPIO_UVC_ITEM_DELAY_OFF,
-  GPIO_UVC_ITEM_DELAY_ON
-} gpio_uvc_item_delay_e;
+  I2C_UVC_ITEM_DELAY_OFF,
+  I2C_UVC_ITEM_DELAY_ON
+} i2c_uvc_item_delay_e;
 
-
-`endif // GPIO_UVC_TYPES_SV 
+`endif // I2C_UVC_TYPES_SV 
