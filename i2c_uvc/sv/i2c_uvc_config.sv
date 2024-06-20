c6033940c13bdd378d4181a0a6ba0f0c0fb9cf08
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-20 13:15:30 +0200

    chore update i2c_ucx_config

--- i2c_uvc/sv/i2c_uvc_config.sv
+++ i2c_uvc/sv/i2c_uvc_config.sv
@@ -1,28 +1,18 @@
-`ifndef GPIO_UVC_CONFIG_SV
-`define GPIO_UVC_CONFIG_SV
+`ifndef I2C_UVC_CONFIG_SV
+`define I2C_UVC_CONFIG_SV
 
-class gpio_uvc_config extends uvm_object;
+class i2c_uvc_config extends uvm_object;
 
-  `uvm_object_utils(gpio_uvc_config)
+  `uvm_object_utils(i2c_uvc_config)
 
   uvm_active_passive_enum  is_active = UVM_ACTIVE;
-  int                      pin_width = 8;
-  local gpio_uvc_data_t    mask;
 
   extern function new(string name = "");
-  extern function gpio_uvc_data_t get_mask();
 
-endclass : gpio_uvc_config
+endclass : i2c_uvc_config
 
-function gpio_uvc_config::new(string name = "");
+function i2c_uvc_config::new(string name = "");
   super.new(name);
 endfunction : new
 
-function gpio_uvc_data_t gpio_uvc_config::get_mask();
-  foreach (mask[i]) begin
-    mask[i] = (i < pin_width);
-  end
-  return mask;
-endfunction : get_mask
-
-`endif // GPIO_UVC_CONFIG_SV
+`endif // I2C_UVC_CONFIG_SV
