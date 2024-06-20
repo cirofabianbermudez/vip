472879d83da3ed127ce62b4db7bf67dc79d54fc0
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-20 13:18:29 +0200

    chore: update i2c_uvc_agent

--- i2c_uvc/sv/i2c_uvc_agent.sv
+++ i2c_uvc/sv/i2c_uvc_agent.sv
@@ -1,43 +1,43 @@
-`ifndef GPIO_UVC_AGENT_SV
-`define GPIO_UVC_AGENT_SV
+`ifndef I2C_UVC_AGENT_SV
+`define I2C_UVC_AGENT_SV
 
-class gpio_uvc_agent extends uvm_agent;
+class i2c_uvc_agent extends uvm_agent;
 
-  `uvm_component_utils(gpio_uvc_agent)
+  `uvm_component_utils(i2c_uvc_agent)
 
-  uvm_analysis_port #(gpio_uvc_sequence_item) analysis_port;
+  uvm_analysis_port #(i2c_uvc_sequence_item) analysis_port;
 
-  gpio_uvc_sequencer sqr;
-  gpio_uvc_driver    drv;
-  gpio_uvc_monitor   mon;
-  gpio_uvc_config    cfg;
+  i2c_uvc_sequencer sqr;
+  i2c_uvc_driver    drv;
+  i2c_uvc_monitor   mon;
+  i2c_uvc_config    cfg;
 
   extern function new(string name, uvm_component parent);
   extern function void build_phase(uvm_phase phase);
   extern function void connect_phase(uvm_phase phase);
 
-endclass : gpio_uvc_agent
+endclass : i2c_uvc_agent
 
 
-function  gpio_uvc_agent::new(string name, uvm_component parent);
+function  i2c_uvc_agent::new(string name, uvm_component parent);
   super.new(name, parent);
 endfunction : new
 
 
-function void gpio_uvc_agent::build_phase(uvm_phase phase);
+function void i2c_uvc_agent::build_phase(uvm_phase phase);
 
-  if ( !uvm_config_db #(gpio_uvc_config)::get(this, "", "cfg", cfg) ) begin
-		  `uvm_fatal(get_name(), "Could not retrieve gpio_uvc_config from config db")
+  if ( !uvm_config_db #(i2c_uvc_config)::get(this, "", "cfg", cfg) ) begin
+		  `uvm_fatal(get_name(), "Could not retrieve i2c_uvc_config from config db")
 	end
 
   if (cfg.is_active == UVM_ACTIVE) begin
-    sqr = gpio_uvc_sequencer::type_id::create("sqr", this);
-    drv = gpio_uvc_driver   ::type_id::create("drv", this);
+    sqr = i2c_uvc_sequencer::type_id::create("sqr", this);
+    drv = i2c_uvc_driver   ::type_id::create("drv", this);
     `uvm_info(get_type_name(), "sqr created", UVM_MEDIUM)
     `uvm_info(get_type_name(), "drv created", UVM_MEDIUM)
   end
 
-  mon = gpio_uvc_monitor::type_id::create("mon", this);
+  mon = i2c_uvc_monitor::type_id::create("mon", this);
   `uvm_info(get_type_name(), "mon created", UVM_MEDIUM)
 
   analysis_port = new("analysis_port", this);
@@ -45,7 +45,7 @@ function void gpio_uvc_agent::build_phase(uvm_phase phase);
 endfunction : build_phase
 
 
-function void gpio_uvc_agent::connect_phase(uvm_phase phase);
+function void i2c_uvc_agent::connect_phase(uvm_phase phase);
 
   if (cfg.is_active == UVM_ACTIVE) begin
     drv.seq_item_port.connect(sqr.seq_item_export);
@@ -57,4 +57,4 @@ function void gpio_uvc_agent::connect_phase(uvm_phase phase);
 endfunction : connect_phase
 
 
-`endif // GPIO_UVC_AGENT_SV
+`endif // I2C_UVC_AGENT_SV
