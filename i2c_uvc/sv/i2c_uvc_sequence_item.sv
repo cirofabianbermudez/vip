b5203ced81d1c182a2c0a6722faf7303d073fa46
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-20 13:02:50 +0200

    chore: make sequence_item for i2c_uvc

--- i2c_uvc/sv/i2c_uvc_sequence_item.sv
+++ i2c_uvc/sv/i2c_uvc_sequence_item.sv
@@ -1,15 +1,23 @@
-`ifndef GPIO_UVC_SEQUENCE_ITEM_SV
-`define GPIO_UVC_SEQUENCE_ITEM_SV
+`ifndef I2C_UVC_SEQUENCE_ITEM_SV
+`define I2C_UVC_SEQUENCE_ITEM_SV
 
-class gpio_uvc_sequence_item extends uvm_sequence_item;
+class i2c_uvc_sequence_item extends uvm_sequence_item;
 
-  `uvm_object_utils(gpio_uvc_sequence_item)
+  `uvm_object_utils(i2c_uvc_sequence_item)
 
-  gpio_uvc_item_stage_e  trans_stage        = GPIO_UVC_ITEM_MIDDLE;
-  gpio_uvc_item_type_e   trans_type         = GPIO_UVC_ITEM_SYNC;
-  gpio_uvc_item_delay_e  delay_enable       = GPIO_UVC_ITEM_DELAY_OFF;
-  rand int unsigned      delay_duration_ps;
-  rand gpio_uvc_data_t   gpio_pin;
+  i2c_uvc_item_stage_e  trans_stage        = I2C_UVC_ITEM_MIDDLE;
+  i2c_uvc_item_type_e   trans_type         = I2C_UVC_ITEM_SYNC;
+  i2c_uvc_item_delay_e  delay_enable       = I2C_UVC_ITEM_DELAY_OFF;
+  rand int unsigned     delay_duration_ps;
+
+  rand logic [6:0] address;
+  rand logic       rw_mode;
+  rand logic [7:0] write_data[$];
+  rand int         num_bytes_read;
+  rand bit         stop_bit_received;
+
+  logic      [7:0] read_data[$];
+  logic            nack;
 
   extern function new(string name = "");
   extern function void do_copy(uvm_object rhs);
@@ -21,46 +29,55 @@ class gpio_uvc_sequence_item extends uvm_sequence_item;
     soft delay_duration_ps inside { [1_000 : 10_000] }; // 1ns - 10ns
   }
 
-endclass : gpio_uvc_sequence_item
+endclass : i2c_uvc_sequence_item
 
 
-function gpio_uvc_sequence_item::new(string name = "");
+function i2c_uvc_sequence_item::new(string name = "");
   super.new(name);
 endfunction
 
 
-function void gpio_uvc_sequence_item::do_copy(uvm_object rhs);
-  gpio_uvc_sequence_item rhs_;
+function void i2c_uvc_sequence_item::do_copy(uvm_object rhs);
+  i2c_uvc_sequence_item rhs_;
   if (!$cast(rhs_, rhs)) begin
     `uvm_fatal(get_type_name(), "Cast of rhs object failed")
   end
   super.do_copy(rhs);
-  gpio_pin = rhs_.gpio_pin;
+
+  address = rhs_.address;
+  rw_mode = rhs_.rw_mode;
+  write_data = rhs_.write_data;
+  num_bytes_read = rhs_.num_bytes_read;
+  stop_bit_received = rhs_.stop_bit_received;
+  read_data = rhs_.read_data;
+  nack = rhs_.nack;
+
   trans_stage = rhs_.trans_stage;
   trans_type = rhs_.trans_type;
   delay_enable = rhs_.delay_enable;
   delay_duration_ps = rhs_.delay_duration_ps;
+
 endfunction : do_copy
 
 
-function bit gpio_uvc_sequence_item::do_compare(uvm_object rhs, uvm_comparer comparer);
+function bit i2c_uvc_sequence_item::do_compare(uvm_object rhs, uvm_comparer comparer);
   bit result;
-  gpio_uvc_sequence_item rhs_;
+  i2c_uvc_sequence_item rhs_;
   if (!$cast(rhs_, rhs)) begin
     `uvm_fatal(get_type_name(), "Cast of rhs object failed")
   end
   result = super.do_compare(rhs, comparer);
-  result &= (gpio_pin == rhs_.gpio_pin);
+  result &= (address == rhs_.address);
   return result;
 endfunction : do_compare
 
 
-function string gpio_uvc_sequence_item::convert2string();
+function string i2c_uvc_sequence_item::convert2string();
   string s;
   s = super.convert2string();
-  $sformat(s, "gpio_pin = 'd%0d, 'h%0h", gpio_pin, gpio_pin);
+  $sformat(s, "address = 'h%0h, rw_mode: %0d, write_data: 'h%0h", address, rw_mode, write_data);
   return s;
 endfunction : convert2string
 
 
-`endif // GPIO_UVC_SEQUENCE_ITEM_SV
+`endif // I2C_UVC_SEQUENCE_ITEM_SV
