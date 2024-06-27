0db0f0b87949886141104abc835442a2d01a9f2c
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-27 12:33:23 +0200

    fix: change variables to i2c_uvc

--- i2c_uvc/vip_vrf/env/top_env.sv
+++ i2c_uvc/vip_vrf/env/top_env.sv
@@ -5,17 +5,8 @@ class top_env extends uvm_env;
 
   `uvm_component_utils(top_env)
 
-  gpio_uvc_agent   port_a_agent;
-  gpio_uvc_config  port_a_agent_cfg;
-
-  gpio_uvc_agent   port_b_agent;
-  gpio_uvc_config  port_b_agent_cfg;
-
-  gpio_uvc_agent   port_c_agent;
-  gpio_uvc_config  port_c_agent_cfg;
-
-  gpio_uvc_agent   port_rst_agent;
-  gpio_uvc_config  port_rst_agent_cfg;
+  i2c_uvc_agent   i2c_agent;
+  i2c_uvc_config  i2c_agent_cfg;
 
   extern function new(string name, uvm_component parent);
   extern function void build_phase(uvm_phase phase);
@@ -41,29 +32,10 @@ endfunction : connect_phase
 
 function void top_env::build_agents();
 
-  port_a_agent_cfg = gpio_uvc_config::type_id::create("port_a_agent_cfg", this);
-  port_a_agent_cfg.is_active = UVM_ACTIVE;
-  port_a_agent_cfg.pin_width = 8;
-  uvm_config_db #(gpio_uvc_config)::set(this, "port_a_agent", "cfg", port_a_agent_cfg);
-  port_a_agent = gpio_uvc_agent::type_id::create("port_a_agent", this);
-
-  port_b_agent_cfg = gpio_uvc_config::type_id::create("port_b_agent_cfg", this);
-  port_b_agent_cfg.is_active = UVM_ACTIVE;
-  port_b_agent_cfg.pin_width = 8;
-  uvm_config_db #(gpio_uvc_config)::set(this, "port_b_agent", "cfg", port_b_agent_cfg);
-  port_b_agent = gpio_uvc_agent::type_id::create("port_b_agent", this);
-
-  port_c_agent_cfg = gpio_uvc_config::type_id::create("port_c_agent_cfg", this);
-  port_c_agent_cfg.is_active = UVM_PASSIVE;
-  port_c_agent_cfg.pin_width = 8;
-  uvm_config_db #(gpio_uvc_config)::set(this, "port_c_agent", "cfg", port_c_agent_cfg);
-  port_c_agent = gpio_uvc_agent::type_id::create("port_c_agent", this);
-
-  port_rst_agent_cfg = gpio_uvc_config::type_id::create("port_rst_agent_cfg", this);
-  port_rst_agent_cfg.is_active = UVM_ACTIVE;
-  port_rst_agent_cfg.pin_width = 1;
-  uvm_config_db #(gpio_uvc_config)::set(this, "port_rst_agent", "cfg", port_rst_agent_cfg);
-  port_rst_agent = gpio_uvc_agent::type_id::create("port_rst_agent", this);
+  i2c_agent_cfg = i2c_uvc_config::type_id::create("i2c_agent_cfg", this);
+  i2c_agent_cfg.is_active = UVM_ACTIVE;
+  uvm_config_db #(i2c_uvc_config)::set(this, "i2c_agent", "cfg", i2c_agent_cfg);
+  i2c_agent = i2c_uvc_agent::type_id::create("i2c_agent", this);
 
 endfunction : build_agents
 
