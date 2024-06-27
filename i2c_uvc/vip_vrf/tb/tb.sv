b00bb2c8d668cef760bac137f7edddeb25bda5ae
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-27 12:30:42 +0200

    fix: i2c_master instantiation, manual rst, interface

--- i2c_uvc/vip_vrf/tb/tb.sv
+++ i2c_uvc/vip_vrf/tb/tb.sv
@@ -4,36 +4,40 @@ module tb;
 
   // Clock signal
   parameter CLK_PERIOD = 10ns;
-  logic clk;
-  always #(CLK_PERIOD / 2) clk = ~clk;
+  logic clk_i = 0;
+  always #(CLK_PERIOD / 2) clk_i = ~clk_i;
 
+  // Reset signal
+  logic rst_i;
   initial begin
-    clk = 0;
+    rst_i = 1; #10ns;
+    rst_i = 0; #10ns;
   end
 
-  gpio_uvc_if port_a_if(clk);
-  gpio_uvc_if port_b_if(clk);
-  gpio_uvc_if port_c_if(clk);
-  gpio_uvc_if port_rst_if(clk);
+  // Interface
+  i2c_uvc_if i2c_master_if(clk_i, rst_i);
 
-  localparam WIDTH = 8;
-
-  adder dut (
-    .clk(port_a_if.clk),
-    .rst(port_rst_if.gpio_pin[0]),
-    .A(port_a_if.gpio_pin[WIDTH-1:0]),
-    .B(port_b_if.gpio_pin[WIDTH-1:0]),
-    .C(port_c_if.gpio_pin_passive[WIDTH-1:0])
+  // Instantiation
+  i2c_master (
+    .clk_i(i2c_master_if.clk_i),
+    .rst_i(i2c_master_if.rst_i),
+    .din_i(i2c_master_if.din_i),
+    .dvsr_i(i2c_master_if.dvsr_i),
+    .cmd_i(i2c_master_if.cmd_i),
+    .wr_i2c_i(i2c_master_if.wr_i2c_i),
+    .scl_io(i2c_master_if.scl_io),
+    .sda_io(i2c_master_if.sda_io),
+    .ready_o(i2c_master_if.ready_o),
+    .done_tick_o(i2c_master_if.done_tick_o),
+    .ack_o(i2c_master_if.ack_o),
+    .dout_o(i2c_master_if.dout_o)
   );
-  
+
   initial begin
     //$timeformat(-9,  0, "ns", 10);
     $timeformat(-12, 0, "ps", 10);
     $fsdbDumpvars;
-    uvm_config_db #(virtual gpio_uvc_if)::set(null, "uvm_test_top.env.port_a_agent", "vif", port_a_if);
-    uvm_config_db #(virtual gpio_uvc_if)::set(null, "uvm_test_top.env.port_b_agent", "vif", port_b_if);
-    uvm_config_db #(virtual gpio_uvc_if)::set(null, "uvm_test_top.env.port_c_agent", "vif", port_c_if);
-    uvm_config_db #(virtual gpio_uvc_if)::set(null, "uvm_test_top.env.port_rst_agent", "vif", port_rst_if);
+    uvm_config_db #(virtual i2c_uvc_if)::set(null, "uvm_test_top.env.i2c_agent", "vif", i2c_master_if);
     run_test();
   end
 
