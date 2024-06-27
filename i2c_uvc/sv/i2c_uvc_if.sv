106fda5b3b10ca4ed5622d4fd9c54f6a1d69d1b9
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-27 12:26:55 +0200

    fix: improve i2c_uvc_if.sv

--- i2c_uvc/sv/i2c_uvc_if.sv
+++ i2c_uvc/sv/i2c_uvc_if.sv
@@ -1,29 +1,46 @@
-`ifndef GPIO_UVC_IF_SV
-`define GPIO_UVC_IF_SV
+`ifndef I2C_UVC_IF_SV
+`define I2C_UVC_IF_SV
 
-interface gpio_uvc_if (
-  input logic clk
+interface i2c_uvc_if (
+  input clk_i,
+  input rst_i
 );
 
-  import gpio_uvc_pkg::*;
+  logic [ 7:0] din_i;
+  logic [15:0] dvsr_i;
+  logic [ 2:0] cmd_i;
+  logic        wr_i2c_i;
+  wire         scl_io;
+  wire         sda_io;
+  logic        ready_o;
+  logic        done_tick_o;
+  logic        ack_o;
+  logic [ 7:0] dout_o;
 
-  gpio_uvc_data_t  gpio_pin;
-  gpio_uvc_data_t  gpio_pin_passive;
+  assign (pull1, highz0) scl_io = 1'b1;
+  assign (pull1, highz0) sda_io = 1'b1;
 
   clocking cb_drv @(posedge clk);
     default output #1ns;
-    output gpio_pin;
+    output din_i;
+    output dvsr_i;
+    output cmd_i;
+    output wr_i2c_i;
+    inout  scl_io;
+    inout  sda_io;
   endclocking : cb_drv
 
   clocking cb_mon @(posedge clk);
     default input #1ns;
-    input gpio_pin;
-    input gpio_pin_passive;
+    input  ready_o;
+    input  done_tick_o;
+    input  ack_o;
+    input  dout_o;
   endclocking : cb_mon
 
-  modport drv (clocking cb_drv, output gpio_pin);
-  modport mon (clocking cb_mon, input gpio_pin, input gpio_pin_passive);
+  modport drv (clocking cb_drv);
+  modport mon (clocking cb_mon);
 
-endinterface : gpio_uvc_if
+endinterface : i2c_uvc_if
 
-`endif // GPIO_UVC_IF_SV
+`endif // I2C_UVC_IF_SV
