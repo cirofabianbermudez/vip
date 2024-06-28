a7b16bf688ec8560bbf87c912e39bcec184858de
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-28 12:18:50 +0200

    feat: add driver and monitor functionality

--- i2c_uvc/sv/i2c_uvc_driver.sv
+++ i2c_uvc/sv/i2c_uvc_driver.sv
@@ -1,41 +1,42 @@
-`ifndef GPIO_UVC_DRIVER_SV
-`define GPIO_UVC_DRIVER_SV
+`ifndef I2C_UVC_DRIVER_SV
+`define I2C_UVC_DRIVER_SV
 
-class gpio_uvc_driver extends uvm_driver #(gpio_uvc_sequence_item);
+class i2c_uvc_driver extends uvm_driver #(i2c_uvc_sequence_item);
 
-  `uvm_component_utils(gpio_uvc_driver)
+  `uvm_component_utils(i2c_uvc_driver)
 
-  virtual gpio_uvc_if vif;
-  gpio_uvc_config    cfg;
+  virtual i2c_uvc_if vif;
+  i2c_uvc_config     cfg;
 
   extern function new(string name, uvm_component parent);
   extern function void build_phase(uvm_phase phase);
   extern task run_phase(uvm_phase phase);
+
   extern task drive_sync();
   extern task drive_async();
   extern task do_drive();
 
-endclass : gpio_uvc_driver
+endclass : i2c_uvc_driver
 
 
-function gpio_uvc_driver::new(string name, uvm_component parent);
+function i2c_uvc_driver::new(string name, uvm_component parent);
   super.new(name, parent);
 endfunction : new
 
 
-function void gpio_uvc_driver::build_phase(uvm_phase phase);
-  if ( !uvm_config_db #(virtual gpio_uvc_if)::get(get_parent(), "", "vif", vif) ) begin
-		  `uvm_fatal(get_name(), "Could not retrieve gpio_uvc_if from config db")
+function void i2c_uvc_driver::build_phase(uvm_phase phase);
+  if ( !uvm_config_db #(virtual i2c_uvc_if)::get(get_parent(), "", "vif", vif) ) begin
+		  `uvm_fatal(get_name(), "Could not retrieve i2c_uvc_if from config db")
 	end
 
-  if ( !uvm_config_db #(gpio_uvc_config)::get(get_parent(), "", "cfg", cfg) ) begin
-		  `uvm_fatal(get_name(), "Could not retrieve gpio_uvc_config from config db")
+  if ( !uvm_config_db #(i2c_uvc_config)::get(get_parent(), "", "cfg", cfg) ) begin
+		  `uvm_fatal(get_name(), "Could not retrieve i2c_uvc_config from config db")
 	end
 
 endfunction : build_phase
 
 
-task gpio_uvc_driver::run_phase(uvm_phase phase);
+task i2c_uvc_driver::run_phase(uvm_phase phase);
   forever begin
     seq_item_port.get_next_item(req);
     do_drive();
@@ -44,16 +45,24 @@ task gpio_uvc_driver::run_phase(uvm_phase phase);
 endtask : run_phase
 
 
-task gpio_uvc_driver::drive_sync();
+task i2c_uvc_driver::drive_sync();
   @(vif.cb_drv);
-  vif.cb_drv.gpio_pin <= req.gpio_pin;
+  vif.cb_drv.rst_i    <= req.rst;
+  vif.cb_drv.dvsr_i   <= cfg.dvsr;
+  vif.cb_drv.din_i    <= req.data_in;
+  vif.cb_drv.wr_i2c_i <= req.write_en;
+  vif.cb_drv.cmd_i    <= req.cmd;
 endtask : drive_sync
 
 
-task gpio_uvc_driver::drive_async();
-  vif.gpio_pin = req.gpio_pin;
-  if (req.delay_enable == GPIO_UVC_ITEM_DELAY_ON) begin
-    $display($sformatf("\ngpio_pin: %5d, delay_enable: %5d, delay_duration_ps: %5d\n",req.gpio_pin, req.delay_enable, req.delay_duration_ps) );
+task i2c_uvc_driver::drive_async();
+  vif.rst_i    = req.rst;
+  vif.dvsr_i   = cfg.dvsr;
+  vif.din_i    = req.data_in;
+  vif.wr_i2c_i = req.write_en;
+  vif.cmd_i    = req.cmd;
+  if (req.delay_enable == I2C_UVC_ITEM_DELAY_ON) begin
+    $display($sformatf("\ndelay_enable: %5d, delay_duration_ps: %5d\n", req.delay_enable, req.delay_duration_ps) );
     #(req.delay_duration_ps*1ps);
   end else begin
     @(vif.cb_drv);
@@ -61,18 +70,26 @@ task gpio_uvc_driver::drive_async();
 endtask : drive_async
 
 
-task gpio_uvc_driver::do_drive();
+task i2c_uvc_driver::do_drive();
 
-  if (req.trans_type == GPIO_UVC_ITEM_ASYNC) begin
+  if (req.trans_type == I2C_UVC_ITEM_ASYNC) begin
     drive_async();
   end else begin
     drive_sync();
   end
 
-  if (req.trans_stage == GPIO_UVC_ITEM_LAST) begin
-    repeat(2) @(vif.cb_drv);
+  if (req.write_en) begin
+    @(vif.cb_drv);
+    vif.cb_drv.wr_i2c_i <= 1'b0;
+    wait (vif.ready_o != 0);
+    @(vif.cb_drv iff (vif.ready_o == 1) );
+    @(vif.cb_drv);
+  end
+
+  if (req.trans_stage == I2C_UVC_ITEM_LAST) begin
+    repeat(100) @(vif.cb_drv);
   end
 
 endtask : do_drive
 
-`endif // GPIO_UVC_DRIVER_SV
+`endif // I2C_UVC_DRIVER_SV
--- i2c_uvc/sv/i2c_uvc_monitor.sv
+++ i2c_uvc/sv/i2c_uvc_monitor.sv
@@ -1,35 +1,35 @@
-`ifndef GPIO_UVC_MONITOR_SV
-`define GPIO_UVC_MONITOR_SV
+`ifndef I2C_UVC_MONITOR_SV
+`define I2C_UVC_MONITOR_SV
 
-class gpio_uvc_monitor extends uvm_monitor;
+class i2c_uvc_monitor extends uvm_monitor;
 
-  `uvm_component_utils(gpio_uvc_monitor)
+  `uvm_component_utils(i2c_uvc_monitor)
 
-  virtual gpio_uvc_if vif;
-  gpio_uvc_config    cfg;
-  uvm_analysis_port #(gpio_uvc_sequence_item) analysis_port;
-  gpio_uvc_sequence_item trans;
+  virtual i2c_uvc_if vif;
+  i2c_uvc_config     cfg;
+  uvm_analysis_port #(i2c_uvc_sequence_item) analysis_port;
+  i2c_uvc_sequence_item trans;
 
   extern function new(string name, uvm_component parent);
   extern function void build_phase(uvm_phase phase);
   extern task run_phase(uvm_phase phase);
   extern task do_mon();
 
-endclass : gpio_uvc_monitor
+endclass : i2c_uvc_monitor
 
 
-function gpio_uvc_monitor::new(string name, uvm_component parent);
+function i2c_uvc_monitor::new(string name, uvm_component parent);
   super.new(name, parent);
 endfunction : new
 
 
-function void gpio_uvc_monitor::build_phase(uvm_phase phase);
-  if ( !uvm_config_db #(virtual gpio_uvc_if)::get(get_parent(), "", "vif", vif) ) begin
-		  `uvm_fatal(get_name(), "Could not retrieve gpio_uvc_if from config db")
+function void i2c_uvc_monitor::build_phase(uvm_phase phase);
+  if ( !uvm_config_db #(virtual i2c_uvc_if)::get(get_parent(), "", "vif", vif) ) begin
+		  `uvm_fatal(get_name(), "Could not retrieve i2c_uvc_if from config db")
 	end
 
-  if ( !uvm_config_db #(gpio_uvc_config)::get(get_parent(), "", "cfg", cfg) ) begin
-		  `uvm_fatal(get_name(), "Could not retrieve gpio_uvc_config from config db")
+  if ( !uvm_config_db #(i2c_uvc_config)::get(get_parent(), "", "cfg", cfg) ) begin
+		  `uvm_fatal(get_name(), "Could not retrieve i2c_uvc_config from config db")
 	end
 
   analysis_port = new("analysis_port", this);
@@ -37,21 +37,16 @@ function void gpio_uvc_monitor::build_phase(uvm_phase phase);
 endfunction : build_phase
 
 
-task gpio_uvc_monitor::run_phase(uvm_phase phase);
-  trans = gpio_uvc_sequence_item::type_id::create("trans");
+task i2c_uvc_monitor::run_phase(uvm_phase phase);
+  trans = i2c_uvc_sequence_item::type_id::create("trans");
   do_mon();
 endtask : run_phase
 
 
-task gpio_uvc_monitor::do_mon();
+task i2c_uvc_monitor::do_mon();
   forever begin
     if (cfg.is_active) begin
-      @(vif.gpio_pin)
-      trans.gpio_pin = vif.gpio_pin;
-    end else begin
-      @(vif.gpio_pin_passive)
-      trans.gpio_pin = vif.gpio_pin_passive & cfg.get_mask();
-      //$display("Value: %b\nMask:  %b\nAND:   %b", vif.gpio_pin_passive, cfg.get_mask(), trans.gpio_pin);
+      @(vif.sda_io);
     end
     `uvm_info(get_type_name(), {"Got item ", trans.convert2string()}, UVM_MEDIUM)
     analysis_port.write(trans);
@@ -60,4 +55,4 @@ endtask : do_mon
 
 
 
-`endif // GPIO_UVC_MONITOR_SV
+`endif // I2C_UVC_MONITOR_SV
