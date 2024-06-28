3dbaae0befc580ddfcf0170ca115a10cf2cdf73f
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-06-28 12:16:04 +0200

    feat: add typedef to handle i2c commands

--- i2c_uvc/sv/i2c_uvc_types.sv
+++ i2c_uvc/sv/i2c_uvc_types.sv
@@ -17,4 +17,12 @@ typedef enum {
   I2C_UVC_ITEM_DELAY_ON
 } i2c_uvc_item_delay_e;
 
+typedef enum logic [2:0] {
+  I2C_UVC_ITEM_START_CMD   = 3'b000,
+  I2C_UVC_ITEM_WR_CMD      = 3'b001,
+  I2C_UVC_ITEM_RS_CMD      = 3'b010,
+  I2C_UVC_ITEM_STOP_CMD    = 3'b011,
+  I2C_UVC_ITEM_RESTART_CMD = 3'b100
+} i2c_uvc_item_cmd_e;
+
 `endif // I2C_UVC_TYPES_SV 
