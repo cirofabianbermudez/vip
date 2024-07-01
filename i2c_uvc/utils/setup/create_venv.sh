7788c65c94243103914b2ed7d34b50ddbd209be9
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-07-01 19:20:03 +0200

    feat: automate format with verible and script

--- i2c_uvc/utils/setup/create_venv.sh
+++ i2c_uvc/utils/setup/create_venv.sh
@@ -32,14 +32,14 @@ else
   exit 1
 fi
 
-echo -e "\n[INFO]: Creating Virtual Environment"
-rm -rf $UTILS_DIR/venv
-/usr/bin/python3 -m venv $UTILS_DIR/venv
-source $UTILS_DIR/venv/bin/activate
-set +e
-pip install --upgrade pip
-set -e
-pip install -r .python_requirements
+#echo -e "\n[INFO]: Creating Virtual Environment"
+#rm -rf $UTILS_DIR/venv
+#/usr/bin/python3 -m venv $UTILS_DIR/venv
+#source $UTILS_DIR/venv/bin/activate
+#set +e
+#pip install --upgrade pip
+#set -e
+#pip install -r .python_requirements
 
 if [ `uname -p` = "x86_64" ]; then
   echo -e "\n[INFO]: Installing Verible"
@@ -47,12 +47,42 @@ if [ `uname -p` = "x86_64" ]; then
   mkdir -p $UTILS_DIR/verible
   cd $UTILS_DIR/verible
   VERIBLE_VER=v0.0-3716-g914652db
-  wget -q https://github.com/chipsalliance/verible/releases/download/${VERIBLE_VER}/verible-${VERIBLE_VER}-linux-static-x86_64.tar.gz
-  tar -xzf verible*.tar.gz
-  rm -rf verible*.tar.gz
+  #wget -q https://github.com/chipsalliance/verible/releases/download/${VERIBLE_VER}/verible-${VERIBLE_VER}-linux-static-x86_64.tar.gz
+  #tar -xzf verible*.tar.gz
+  #rm -rf verible*.tar.gz
   echo "export PATH=$UTILS_DIR/verible/verible-${VERIBLE_VER}/bin:\$PATH" >> $UTILS_DIR/venv/bin/activate
   echo "setenv PATH $UTILS_DIR/verible/verible-${VERIBLE_VER}/bin:\$PATH" >> $UTILS_DIR/venv/bin/activate.csh
+  echo -e "\n[INFO]: Creating format flow"
+  rm -rf $UTILS_DIR/format
+  mkdir -p $UTILS_DIR/format
+  cd $UTILS_DIR/format
+
+  SV_FILES=($(find ../../sv ../../vip_vrf -type f -name "*.sv"))
+  NUM_ELEM=${#SV_FILES[@]}
+
+  echo -e "ROOT_DIR = \$(realpath \$(CURDIR)/../../)\n" >> Makefile
+  echo -e "SRCS = \\" >> Makefile
+
+  if [ $NUM_ELEM -gt 0 ]; then
+    for ((i = 0; i < ${NUM_ELEM}; i++)) do
+      temp=$(echo "${SV_FILES[i]}" | sed 's/\.\.\/\.\./$(ROOT_DIR)/g')
+      if [ $i -eq $(($NUM_ELEM -1)) ]; then
+        printf "\t%s\n" "$temp" >> Makefile
+      else
+        printf "\t%s\\" "$temp" >> Makefile
+        printf "\n" >> Makefile
+      fi
+    done
+  else
+    echo "[WARNING]: No .sv file found"
+  fi
+  echo -e "\n.PHONY: format preview lint clean\n\nall: format\n\nformat:\n\tverible-verilog-format --inplace \$(SRCS)" >> Makefile
+  echo -e "\npreview:\n\trm -rf preview\n\tmkdir -p preview\n\t\$(foreach file, \$(SRCS), verible-verilog-format \$(file) > preview/format_\$(notdir \$(file)); )" >> Makefile
+  echo -e "\nlint:\n\tverible-verilog-lint \$(SRCS) || true" >> Makefile
+  echo -e "\nclean:\n\trm -rf" preview>> Makefile
+  echo -e "" >> Makefile
 else
   echo "[WARNING]: Skiping Verible installation"
 fi
+
 
