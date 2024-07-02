056d4d5274460e1e46caea47156d368d35ca99f6
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2024-07-02 12:27:39 +0200

    feat: improve create_venv.sh

--- i2c_uvc/utils/setup/create_venv.sh
+++ i2c_uvc/utils/setup/create_venv.sh
@@ -32,14 +32,14 @@ else
   exit 1
 fi
 
-#echo -e "\n[INFO]: Creating Virtual Environment"
-#rm -rf $UTILS_DIR/venv
-#/usr/bin/python3 -m venv $UTILS_DIR/venv
-#source $UTILS_DIR/venv/bin/activate
-#set +e
-#pip install --upgrade pip
-#set -e
-#pip install -r .python_requirements
+echo -e "\n[INFO]: Creating Virtual Environment"
+rm -rf $UTILS_DIR/venv
+/usr/bin/python3 -m venv $UTILS_DIR/venv
+source $UTILS_DIR/venv/bin/activate
+set +e
+pip install --upgrade pip
+set -e
+pip install -r .python_requirements
 
 if [ `uname -p` = "x86_64" ]; then
   echo -e "\n[INFO]: Installing Verible"
@@ -47,12 +47,12 @@ if [ `uname -p` = "x86_64" ]; then
   mkdir -p $UTILS_DIR/verible
   cd $UTILS_DIR/verible
   VERIBLE_VER=v0.0-3716-g914652db
-  #wget -q https://github.com/chipsalliance/verible/releases/download/${VERIBLE_VER}/verible-${VERIBLE_VER}-linux-static-x86_64.tar.gz
-  #tar -xzf verible*.tar.gz
-  #rm -rf verible*.tar.gz
+  wget -q https://github.com/chipsalliance/verible/releases/download/${VERIBLE_VER}/verible-${VERIBLE_VER}-linux-static-x86_64.tar.gz
+  tar -xzf verible*.tar.gz
+  rm -rf verible*.tar.gz
   echo "export PATH=$UTILS_DIR/verible/verible-${VERIBLE_VER}/bin:\$PATH" >> $UTILS_DIR/venv/bin/activate
   echo "setenv PATH $UTILS_DIR/verible/verible-${VERIBLE_VER}/bin:\$PATH" >> $UTILS_DIR/venv/bin/activate.csh
-  echo -e "\n[INFO]: Creating format flow"
+  echo -e "\n[INFO]: Creating Verible Makefile"
   rm -rf $UTILS_DIR/format
   mkdir -p $UTILS_DIR/format
   cd $UTILS_DIR/format
@@ -60,27 +60,69 @@ if [ `uname -p` = "x86_64" ]; then
   SV_FILES=($(find ../../sv ../../vip_vrf -type f -name "*.sv"))
   NUM_ELEM=${#SV_FILES[@]}
 
-  echo -e "ROOT_DIR = \$(realpath \$(CURDIR)/../../)\n" >> Makefile
-  echo -e "SRCS = \\" >> Makefile
+  {
+    echo "ROOT_DIR = \$(realpath \$(CURDIR)/../../)"
+    echo ""
+    echo "SRCS = \\"
 
-  if [ $NUM_ELEM -gt 0 ]; then
     for ((i = 0; i < ${NUM_ELEM}; i++)) do
       temp=$(echo "${SV_FILES[i]}" | sed 's/\.\.\/\.\./$(ROOT_DIR)/g')
       if [ $i -eq $(($NUM_ELEM -1)) ]; then
-        printf "\t%s\n" "$temp" >> Makefile
+        printf "\t%s\n" "$temp"
       else
-        printf "\t%s\\" "$temp" >> Makefile
-        printf "\n" >> Makefile
+        printf "\t%s\\" "$temp"
+        printf "\n"
       fi
     done
-  else
+
+    echo ""
+    echo ".PHONY: format preview lint clean"
+    echo ""
+    echo "all: lint"
+    echo ""
+    echo "format:"
+    echo -e "\tverible-verilog-format --inplace \$(SRCS)"
+    echo ""
+    echo "preview:"
+    echo -e "\trm -rf preview"
+    echo -e "\tmkdir -p preview"
+    echo -e "\t\$(foreach file, \$(SRCS), verible-verilog-format \$(file) > preview/format_\$(notdir \$(file)); )"
+    echo ""
+    echo "lint:"
+    echo -e "\tverible-verilog-lint \$(SRCS) || true"
+    echo ""
+    echo "clean:"
+    echo -e "\trm -rf preview"
+    echo ""
+  } > Makefile
+
+  if [ $NUM_ELEM -eq 0 ]; then
     echo "[WARNING]: No .sv file found"
   fi
-  echo -e "\n.PHONY: format preview lint clean\n\nall: format\n\nformat:\n\tverible-verilog-format --inplace \$(SRCS)" >> Makefile
-  echo -e "\npreview:\n\trm -rf preview\n\tmkdir -p preview\n\t\$(foreach file, \$(SRCS), verible-verilog-format \$(file) > preview/format_\$(notdir \$(file)); )" >> Makefile
-  echo -e "\nlint:\n\tverible-verilog-lint \$(SRCS) || true" >> Makefile
-  echo -e "\nclean:\n\trm -rf" preview>> Makefile
-  echo -e "" >> Makefile
+
+  {
+    echo "--inplace"
+    echo "--line_break_penalty=8"
+    echo "--column_limit=200"
+    echo "--over_column_limit_penalty=100"
+  } > .rules.verible_format
+
+  {
+    echo  "-explicit-parameter-storage-type"
+    echo  "-forbidden-macro"
+    echo  "+line-length=length:250"
+    echo  "+macro-string-concatenation"
+    echo  "+mismatched-labels"
+    echo  "+one-module-per-file"
+    echo  "+parameter-name-style=parameter_style:ALL_CAPS"
+    echo  "+parameter-name-style=localparam_style:ALL_CAPS"
+    echo  "+port-name-suffix"
+    echo  "+proper-parameter-declaration"
+    echo  "+signal-name-style"
+    echo  "+suspicious-semicolon"
+    echo  "+uvm-macro-semicolon"
+  } > .rules.verible_lint
+
 else
   echo "[WARNING]: Skiping Verible installation"
 fi
